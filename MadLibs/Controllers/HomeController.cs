ef28fd9a517f961569bf2922981d915f7c17adf3
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-09-27 14:03:29 -0700

    Add story number

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MadLibs.Models;
 
 namespace MadLibs.Controllers
 {
@@ -9,6 +10,32 @@ namespace MadLibs.Controllers
       return View();
     }
 
+    [Route("/form")]
+    public ActionResult Form(string storyNumber) {
+      Story myStory = new Story(); 
+      myStory.StoryNumber = storyNumber;
+      return View(myStory);
+    }
+
+    [Route("/display")]
+    public ActionResult Display(string adjective, string food1, string verb, string saying, string noun, string food2, string color, string ride, string animal, string person)
+    {
+      //make var
+      MadVariables myVariables = new MadVariables();
+      //set the properties
+      myVariables.Adjective = adjective;
+      myVariables.Food1 = food1;
+      myVariables.Verb = verb;
+      myVariables.Saying = saying;
+      myVariables.Noun = noun;
+      myVariables.Food2 = food2;
+      myVariables.Color = color;
+      myVariables.Ride = ride;
+      myVariables.Animal = animal;
+      myVariables.Person = person;
+      //pass them to view
+      return View(myVariables);
+    }
     /*
     Controller:
       add routes:
